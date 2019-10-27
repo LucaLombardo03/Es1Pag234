81ed9815117f760451bbef1fe4c423624f7c41ee
luca lombardo <lucalombardo03@gmail.com>
2019-10-27 12:11:52 +0100

    Add project files.

---
+++ Es1Pag234/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Es1Pag234
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeri = 0;
+            int somma = 0;
+            Leggi(ref numeri, ref somma);
+            Console.WriteLine($"Hai inserito {numeri} numeri e la somma è {somma}");
+            Console.ReadLine();
+        }
+
+        private static void Leggi(ref int count, ref int somma)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            if (numero == 0)
+                return;
+            count++;
+            somma += numero;
+            Leggi(ref count, ref somma);
+        }
+    }
+}
